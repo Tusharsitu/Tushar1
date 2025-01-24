5dbde908f5e064cd5aa4d8582bebf7aa74cd523d
Tushar <94365211+Tusharsitu@users.noreply.github.com>
2025-01-24 18:59:24 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "ami_value" {
+   description = "ami value name"
+}
+
+variable "instance_type_value" {
+   description = "instance type"
+}
+
+variable "subnet_id" {
+   description = "subnet range"
+}
